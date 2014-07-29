5b467f5d2a0c83aa5224dd5692ab117262198ae4
leelolo <lilonglongfwz@gmail.com>
2014-07-29 23:33:48 +0800

    [chore]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  #attr_accessible :email, :password, :password_confirmation
   attr_accessor :password
   validates :user_name, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => {:with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?}
@@ -7,10 +8,19 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   after_save :clear_password
 
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.encrypted_password == BCrypt::Engine.hash_secret(password, user.salt)
+      user
+    else
+      nil
+    end
+  end
+
   def encrypt_password
     if password.present?
       self.salt = BCrypt::Engine.generate_salt
-      self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
+      self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)
     end
   end
 
