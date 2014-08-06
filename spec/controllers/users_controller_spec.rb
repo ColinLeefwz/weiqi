21a0c19434444263898fcedd5512b5d31951f9aa
leelolo <lilonglongfwz@gmail.com>
2014-08-06 23:24:59 +0800

    [feature]use factory girl in rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,22 +13,24 @@ RSpec.describe UsersController, :type => :controller do
   describe "POST #create" do
     context 'when user is valid' do
       it "creates a new user" do
-        post 'create', user: {id: 1, email: "lilonglong@weiqi.com", user_name: "lilonglong", password: "123123123"}
+        #post 'create', user: {id: 1, email: "lilonglong@weiqi.com", user_name: "lilonglong", password: "123123123"}
+        post 'create', user: FactoryGirl.attributes_for(:user)
         expect(assigns(:user)).to eq(User.find_by(id:1))
       end
       it "redirects to the home page" do
-        post 'create', user: {id: 1, email: "lilonglong@weiqi.com", user_name: "lilonglong", password: "123123123"}
+        post 'create', user: FactoryGirl.attributes_for(:user)
         expect(response).to redirect_to(root_path)
       end
     end
 
     context 'when user is invalid' do
       it "cannot create a new user" do
-        post 'create', user: {id: 1, email: "", user_name: "lilonglong", password: "123123123"}
+        #post 'create', user: {id: 1, email: "", user_name: "lilonglong", password: "123123123"}
+        post 'create', user: FactoryGirl.attributes_for(:user, email: "")
         expect(assigns(:user)).to_not eq(User.find_by(id:1))
       end
       it "redirects to the home page" do
-        post 'create', user: {id: 1, email: "", user_name: "lilonglong", password: "123123123"}
+        post 'create', user: FactoryGirl.attributes_for(:user, email: "")
         expect(response).to_not redirect_to(root_path)
       end
     end
@@ -36,7 +38,8 @@ RSpec.describe UsersController, :type => :controller do
 
   describe "GET #show/:id" do
     before :each do
-      @user = User.create!(id: 1, email: "lilonglong@weiqi.com", user_name: "lilonglong", password: "123123123")
+      #@user = User.create!(id: 1, email: "lilonglong@weiqi.com", user_name: "lilonglong", password: "123123123")
+      @user = FactoryGirl.create(:user)
     end
 
     it "responds successfully with an HTTP 200 status code" do
